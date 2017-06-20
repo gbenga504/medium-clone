31a2c98ebb54771d51a818cc8ae3dad8c1fd8788
gbenga504 <daveanifowoshe@ymail.com>
2017-06-20 15:47:44 +0100

    made some important chamges to the icons

--- src/screens/News/AltIcon/index.js
+++ src/screens/News/AltIcon/index.js
@@ -4,7 +4,23 @@ import './index.css';
 class AltIcon extends Component{
     render(){
         return (
-            <div></div>
+            <div className="altIcon">
+                <div id="tagName">Heart</div>
+                <div className="iconHolder">
+                    <img src="/images/recommend.png" alt="recommend icon" className="recommend_icon" />
+                    <div className="shareCount">
+                        100
+                    </div>
+                </div>
+                <div className="iconHolder">
+                    <img src="/images/bookmark.png" alt="bookamrk icon" className="recommend_icon" />
+                    <div className="shareCount">
+                        82
+                    </div>
+                </div>
+            </div>
         )
     }
 }
+
+export default AltIcon;
