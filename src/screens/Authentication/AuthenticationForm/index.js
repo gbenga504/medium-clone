b2b1aa12660901bfa864eeee00e08137634434b3
gbenga504 <daveanifowoshe@ymail.com>
2017-06-28 16:15:06 +0100

    hooked the admin loggin page with Fetch and automatic redirect after successful login

--- src/screens/Authentication/AuthenticationForm/index.js
+++ src/screens/Authentication/AuthenticationForm/index.js
@@ -3,13 +3,31 @@ import SubContainer from "../SubContainer";
 import "./index.css";
 
 class AuthenticationForm extends PureComponent {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    }
+    this.updateInputDetails = this.updateInputDetails.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  login(){
+    this.props.login(this.state);
+  }
+
+  updateInputDetails({type,value}){
+    type == 'email' ? this.setState({email:value}) : this.setState({password: value});
+  }
+  
   render() {
     return (
       <SubContainer>
         <FormHeader />
         <TagLine />
-        <SocialRegistrationBtn />
-        <SocialRegistrationBtn />
+        <LoginForm onKeyUp={this.updateInputDetails}/>
+        <LoginBtn onClick={this.login}/>
       </SubContainer>
     );
   }
@@ -27,17 +45,42 @@ const TagLine = () => {
   return (
     <div className="row tagMainHeader">
       <div className="col-xs-12 text-center tagHeadLine">
-        Continue with social authentication to have access to update and make posts
-        {" "}
+        Click Login to continue
       </div>
     </div>
   );
 };
 
-const SocialRegistrationBtn = props => {
+const LoginForm = (props) => {
   return (
-    <div className="row btnContainer">
-      <div className="col-xs-12 col-sm-offset-1 col-sm-10 socialBtn" />
+    <div className="row">
+      <input
+        ref = {(ref) => this.email = ref}
+        type="email"
+        placeholder="Enter your email"
+        className="col-xs-12 col-sm-offset-1 col-sm-10 formElContainer inputEl"
+        onKeyUp={()=> {props.onKeyUp({type:'email', value: this.email.value})}}
+      />
+      <input
+        ref = {(ref) => this.password = ref}
+        type="password"
+        placeholder="Enter your password"
+        className="col-xs-12 col-sm-offset-1 col-sm-10 formElContainer inputEl"
+        onKeyUp={()=> {props.onKeyUp({type:'password', value: this.password.value})}}
+      />
+    </div>
+  );
+};
+
+const LoginBtn = (props) => {
+  return (
+    <div className="row formElContainer">
+      <input
+        type="button"
+        value="Login"
+        onClick={props.onClick}
+        className="col-xs-12 col-sm-offset-1 col-sm-10 formElContainer loginBtn"
+      />
     </div>
   );
 };
--- src/screens/Authentication/index.js
+++ src/screens/Authentication/index.js
@@ -2,8 +2,38 @@ import React, { PureComponent } from "react";
 import { ContainerLayout } from "../../containers/Layout";
 import Header from "./Header";
 import AuthenticationForm from "./AuthenticationForm";
+import {httpFetch} from '../../containers/Request';
+import {withRouter} from 'react-router-dom';
 
 class Authentication extends PureComponent {
+  constructor(props){
+    super(props);
+    this.state = {
+      loginURI : '',
+      data : {},
+    }
+    this.login = this.login.bind(this);
+  }
+
+  login({email, password}){
+    httpFetch(this.state.loginURI, {
+      handleResponseAs: "json",
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }).then (data => {
+        if(data.status == "success"){
+          window.localStorage.setItem('user_details', JSON.stringify(data.data));
+          this.props.history.push('/loggedIn');
+        }
+        else return Promise.reject(data.message);
+    }).catch(error => {
+        console.log("An error occurred while trying to rerieve data");
+    })
+  }
+
   render() {
     return (
       <div className="row">
@@ -12,7 +42,7 @@ class Authentication extends PureComponent {
           <ContainerLayout color="#FBFCFD">
             <div className="row">
               <div className="col-xs-12">
-                <AuthenticationForm />
+                <AuthenticationForm login={this.login}/>
               </div>
             </div>
           </ContainerLayout>
@@ -22,4 +52,4 @@ class Authentication extends PureComponent {
   }
 }
 
-export default Authentication;
+export default withRouter(Authentication);
