edfcb7dee7f2d16a88698754bba1065db265c8bc
gbenga504 <daveanifowoshe@ymail.com>
2017-07-01 20:58:40 +0100

    created stateless components from class components without states

--- src/screens/News/Title/index.js
+++ src/screens/News/Title/index.js
@@ -1,19 +1,17 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import "./index.css";
 
 /**
  * @Component Title renders the title of the news screen 
  */
-class Title extends PureComponent {
-  render() {
-    return (
-      <div className="row title">
-        <div className="col-xs-12 text-center">
-          <span className="postTitle">{this.props.title}</span>
-        </div>
+const Title = () => {
+  return (
+    <div className="row title">
+      <div className="col-xs-12 text-center">
+        <span className="postTitle">{this.props.title}</span>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Title;
--- src/screens/Post/Header/index.js
+++ src/screens/Post/Header/index.js
@@ -1,39 +1,37 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { HeaderLayout } from "../../../containers/Layout";
 import "./index.css";
 
 /**
  * @Component Header presentational container displays the header of the post screen
  */
-class Header extends PureComponent {
-  render() {
-    return (
-      <HeaderLayout border={true} color="#fff">
-        <div className="col-xs-12">
-          <div className="col-sm-4 hidden-xs">
-            <img src="" id="logo" alt="logo" />
-            &nbsp;
-            <span id="saveChanges">Unsaved changes</span>
-          </div>
-          <div className="col-xs-12 col-sm-offset-2 col-sm-6">
-            <div className="section-icon-header-positioner">
-              <div id="header-right-section" className="col-xs-3">
-                share
+const Header = () => {
+  return (
+    <HeaderLayout border={true} color="#fff">
+      <div className="col-xs-12">
+        <div className="col-sm-4 hidden-xs">
+          <img src="" id="logo" alt="logo" />
+          &nbsp;
+          <span id="saveChanges">Unsaved changes</span>
+        </div>
+        <div className="col-xs-12 col-sm-offset-2 col-sm-6">
+          <div className="section-icon-header-positioner">
+            <div id="header-right-section" className="col-xs-3">
+              share
+            </div>
+            <div id="iconHolder" className="col-xs-9">
+              <div id="publishBtn" onClick={this.props.onMakePost}>
+                publish
               </div>
-              <div id="iconHolder" className="col-xs-9">
-                <div id="publishBtn" onClick={this.props.onMakePost}>
-                  publish
-                </div>
-                <div id="profileImage">
-                  <img src="" alt="" id="headerIcons" />
-                </div>
+              <div id="profileImage">
+                <img src="" alt="" id="headerIcons" />
               </div>
             </div>
           </div>
         </div>
-      </HeaderLayout>
-    );
-  }
-}
+      </div>
+    </HeaderLayout>
+  );
+};
 
 export default Header;
